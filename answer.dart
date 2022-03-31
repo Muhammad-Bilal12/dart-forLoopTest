eb35ef3afeaeeda2d334dc1bdcf24780edeaf684
bilal <muhammadbilalmnaeem@gmail.com>
2022-03-31 23:00:14 +0500

    Question1 complete

---
+++ answer.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+// Quetion 2
+  for (var i = 1; i < 5; i++) {
+    for (var j = 0; j < i; j++) {
+      print(i);
+    }
+    print("\n");
+  }
+
+// Question 2 done
+
+//
+}
