a8c4af08372bb75baad9a2a4ebbab8259b78c276
bilal <muhammadbilalmnaeem@gmail.com>
2022-04-01 00:55:18 +0500

    Question 4

--- answer.dart
+++ answer.dart
@@ -22,7 +22,7 @@ void main(List<String> args) {
 
 // Question 3 done
 
-// Question 5
+// Question 6
 
   var char = '@';
   if (char.codeUnitAt(0) >= 97 && char.codeUnitAt(0) <= 122) {
@@ -35,5 +35,36 @@ void main(List<String> args) {
     print("you hit the character of symbol of $char");
   }
 
-// Question 5 done
+// Question 6 done
+
+// Question 4
+
+  List myLst = [1, 3, 3, 7, 4, 3, 2, 3, 3, 3, 3, 5, 6, 7, 3];
+  // var maxCount = 0;
+  // var st = 0;
+
+  var maxCount = 0;
+  var index = -1; // sentinels
+  var n = myLst.length;
+  for (var i = 0; i < n; i++) {
+    var count = 0;
+    for (var j = 0; j < n; j++) {
+      if (myLst[i] == myLst[j]) count++;
+    }
+
+    // Update maxCount if count of
+    // current element is greater
+    if (count > maxCount) {
+      maxCount = count;
+      index = i;
+    }
+  }
+
+  // If maxCount is greater than n/2
+  // return the corresponding element
+  if (maxCount > n / 2)
+    print(myLst[index]);
+  else
+    print("No Majority Element");
+  print(myLst.length);
 }
